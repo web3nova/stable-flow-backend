bd298000af2004fd6699128b3f62e754558b794d
BernardOnuh <onuhbernard4@gmail.com>
2026-04-28 15:14:58 +0100

    config: network=base-sepolia timeout=300000ms

--- main.ts
+++ main.ts
@@ -14,7 +14,7 @@ const CONFIG = {
   RECIPIENT_ADDRESS: "0x75ba0000000000000000000000000000000000000", // Change this!
   VALUE: parseEther("0"),
   DATA: "0x",
-  TIMEOUT_MS: 900000, // 15 minutes
+  TIMEOUT_MS: 300000, // 5 minutes
 };
 
 // Main execution function
