85560ef47e8122448582836a0144d411cf9af7d7
WEB3NOVA <theweb3nova@gmail.com>
2026-04-30 06:28:15 +0100

    update server.ts

--- server.ts
+++ server.ts
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Add CORS if needed
+
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
